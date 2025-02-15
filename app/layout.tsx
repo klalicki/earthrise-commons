bdfbee400802e7171eb14dcdd68d1e7f09f10d26
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2025-02-15 11:12:48 -0500

    sets up carbon design header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import "./globals.css";
+import "./globals.scss";
 import { Inter } from "next/font/google";
-
+import ECHeader from "../components/layout/header/ECHeader";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -15,7 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ECHeader />
+        <main>{children}</main>
+        <footer></footer>
+      </body>
     </html>
   );
 }
