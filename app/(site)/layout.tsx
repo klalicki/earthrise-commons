9142fe3174eb530e3c8db50613fb4b6abaa43903
Kristoff Lalicki <32229646+klalicki@users.noreply.github.com>
2025-02-16 23:07:37 -0500

    fixes fluid grid on pages

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,13 +1,20 @@
+"use client";
 import type { ReactNode } from "react";
 import "./styles.scss";
 
 import ECHeader from "../../components/layout/ECHeader/ECHeader";
+import { Grid, Column } from "@carbon/react";
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
         <ECHeader></ECHeader>
-        {children}
+        <Grid>
+          <Column sm={4} md={6} lg={8} xlg={8}>
+            {children}
+          </Column>
+        </Grid>
       </body>
     </html>
   );
